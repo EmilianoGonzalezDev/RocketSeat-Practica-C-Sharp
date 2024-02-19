9ea95a022b38de2ed9b7bd0d171d7e08f71ba164
Emiliano Gonzalez <EMILIANO_G06@hotmail.com>
2024-02-19 15:49:38 -0300

    initial commit

---
+++ src/RocketseatAuction.API/Repositories/RocketSeatAuctionDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RocketseatAuction.API.Entities;
+
+namespace RocketseatAuction.API.Repositories;
+
+public class RocketSeatAuctionDbContext : DbContext
+{
+    public DbSet<Auction> Auctions { get; set; }
+
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite(@"Data Source=C:\Users\Emiliano G\Downloads\leilaoDbNLW.db");
+    }
+}
---
+++ src/RocketseatAuction.API/UseCases/Auctions/GetCurrent/GetCurrentAuctionUseCase.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RocketseatAuction.API.Entities;
+using RocketseatAuction.API.Repositories;
+
+namespace RocketseatAuction.API.UseCases.Auctions.GetCurrent;
+
+public class GetCurrentAuctionUseCase
+{
+    public Auction? Execute()
+    {
+        var repository = new RocketSeatAuctionDbContext();
+
+        var today = DateTime.Now;
+
+        return repository
+            .Auctions
+            .Include(a => a.Items)
+            .First();
+            //.FirstOrDefault(a => today >= a.Starts && today <= a.Ends);
+    }
+}
